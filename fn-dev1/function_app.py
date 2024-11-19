6a429c80fe210d465af834f8c6de27b82cd06236
Anitha <anitha.sudulagunta@rsystems.com>
2024-11-19 13:15:51 +0530

    testing

--- fn-dev1/function_app.py
+++ fn-dev1/function_app.py
@@ -20,6 +20,6 @@ def http_trigger1(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered1 function executed successfully function for testing123",
+             "This HTTP triggered1 function executed successfully function for testing123456",
              status_code=200
         )
